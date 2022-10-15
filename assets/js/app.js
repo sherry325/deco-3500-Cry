c28d9776c54a154dcea86da2b81b1eb654cdbab6
Nicholas Tseng <nicolas@desygner.com>
2022-10-16 04:08:57 +1000

    Implement timer functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+let timerButton = null;
+
 function navigate (path) {
 	window.location.href = path;
 }
@@ -30,9 +32,10 @@ function saveTask () {
 	hideAddTaskOverlay();
 }
 
-function showTimerOverlay () {	
+function showTimerOverlay (event) {	
 	const timerOverlay = document.querySelector('div.timerOverlay');
 	const overlayBackground = document.querySelector('div.overlayBackground');
+	timerButton = event.target;
 
 	timerOverlay.style.display = 'flex';
 	overlayBackground.style.display = 'initial';
@@ -41,7 +44,26 @@ function showTimerOverlay () {
 function hideTimerOverlay () {
 	const timerOverlay = document.querySelector('div.timerOverlay');
 	const overlayBackground = document.querySelector('div.overlayBackground');
+	timerButton = null;
 
 	timerOverlay.style.display = 'none';
 	overlayBackground.style.display = 'none';
+}
+
+function saveTime () {
+	const input = document.querySelector('input');
+	const errorText = document.querySelector('div.error');
+	const saveButton = document.querySelector('span.overlayButton');
+	const regex = /^[0-9]+$/;
+
+	if (!input.value.match(regex)) {
+		errorText.style.display = 'initial';
+		saveButton.style.marginTop = '1rem';
+	} else {
+		timerButton.querySelector('span.time').innerHTML = input.value;
+		input.value = '';
+		errorText.style.display = 'none';
+		saveButton.style.marginTop = '2rem';
+		hideTimerOverlay();
+	}
 }
